48f3685eff17d581764550c687439ea6b14d9cd8
itsmurasaki <itsmurasaki@gmail.com>
2014-02-20 13:58:37 +0000

    made good use of the array

--- directory.rb
+++ directory.rb
@@ -1,32 +1,56 @@
-student_count = 9
-
 # list of students
-puts "Students in my cohort at MakersAcademy"
-puts "--------------------------------------"
-puts "Mario Gintili"
-puts "Mikhail Dubov"
-puts "Karolis Noreika"
-puts "Michael Sidon"
-puts "Charles De Barros"
-puts "Ruslan Vikhor"
-puts "Toby Retallick"
-puts "Mark Mekhaiel"
-puts "Sarah Young"
-puts "Hannah Wight"
-puts "Khushkaran Singh"
-puts "Rick brunstedt"
-puts "Manjit Singh"
-puts "Alex Gaudiosi"
-puts "Ross Hepburn"
-puts "Natascia Marchese"
-puts "Tiffanie Chia"
-puts "Matthew Thomas"
-puts "Freddy McGroarty"
-puts "Tyler Rollins"
-puts "Richard Curteis"
-puts "Anna Yanova"
-puts "Andrew Cumine" 
-print "\n"
+students = [
+  "Mario Gintili",
+  "Mikhail Dubov",
+  "Karolis Noreika",
+  "Michael Sidon",
+  "Charles De Barros",
+  "Ruslan Vikhor",
+  "Toby Retallick",
+  "Mark Mekhaiel",
+  "Sarah Young",
+  "Hannah Wight",
+  "Khushkaran Singh",
+  "Rick brunstedt",
+  "Manjit Singh",
+  "Alex Gaudiosi",
+  "Ross Hepburn",
+  "Natascia Marchese",
+  "Tiffanie Chia",
+  "Matthew Thomas",
+  "Freddy McGroarty",
+  "Tyler Rollins",
+  "Richard Curteis",
+  "Anna Yanova",
+  "Andrew Cumine"
+]
+
+puts "The students of my cohort at Makers Academy"
+puts "-------------------------------------------"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
+puts students[19]
+puts students[20]
+puts students[21]
+puts students[22]
+puts students[23]
 
 # total students count
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
