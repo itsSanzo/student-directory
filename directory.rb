73e45dc94773eb7d126dd6ffb18d421bdaf995c5
itsmurasaki <itsmurasaki@gmail.com>
2014-02-20 14:23:41 +0000

    added cohort

--- directory.rb
+++ directory.rb
@@ -1,37 +1,47 @@
 # we make a list of the students
 students = [
-  "Mario Gintili",
-  "Mikhail Dubov",
-  "Karolis Noreika",
-  "Michael Sidon",
-  "Charles De Barros",
-  "Ruslan Vikhor",
-  "Toby Retallick",
-  "Mark Mekhaiel",
-  "Sarah Young",
-  "Hannah Wight",
-  "Khushkaran Singh",
-  "Rick brunstedt",
-  "Manjit Singh",
-  "Alex Gaudiosi",
-  "Ross Hepburn",
-  "Natascia Marchese",
-  "Tiffanie Chia",
-  "Matthew Thomas",
-  "Freddy McGroarty",
-  "Tyler Rollins",
-  "Richard Curteis",
-  "Anna Yanova",
-  "Andrew Cumine"
+  ["Mario Gintili", :february],
+  ["Mikhail Dubov", :february],
+  ["Karolis Noreika", :february],
+  ["Michael Sidon", :february],
+  ["Charles De Barro,", :february],
+  ["Ruslan Vikhor", :february],
+  ["Toby Retallick", :february],
+  ["Mark Mekhaiel", :february],
+  ["Sarah Young", :february],
+  ["Hannah Wight", :february],
+  ["Khushkaran Singh", :february],
+  ["Rick brunstedt", :february],
+  ["Manjit Singh", :february],
+  ["Alex Gaudiosi", :february],
+  ["Ross Hepburn", :february],
+  ["Natascia Marches,", :february],
+  ["Tiffanie Chia", :february],
+  ["Matthew Thomas", :february],
+  ["Freddy McGroarty", :february],
+  ["Tyler Rollins", :february],
+  ["Richard Curteis", :february],
+  ["Anna Yanova", :february],
+  ["Andrew Cumine", :february],
 ]
 
 #then we print the list
-puts "The students of my cohort at Makers Academy"
-puts "-------------------------------------------"
 
-students.each do |student|
-	puts student
+def print_header 
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------------------------------------"
 end
 
-# then we count them
-puts "Overall, we have #{students.length} great students"
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
