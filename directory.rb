b81c7f37de64ad5848c2021bc52de57c59f4133a
itsmurasaki <itsmurasaki@gmail.com>
2014-02-20 14:02:50 +0000

    added iteration

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-# list of students
+# we make a list of the students
 students = [
   "Mario Gintili",
   "Mikhail Dubov",
@@ -25,32 +25,13 @@ students = [
   "Andrew Cumine"
 ]
 
+#then we print the list
 puts "The students of my cohort at Makers Academy"
 puts "-------------------------------------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
-puts students[12]
-puts students[13]
-puts students[14]
-puts students[15]
-puts students[16]
-puts students[17]
-puts students[18]
-puts students[19]
-puts students[20]
-puts students[21]
-puts students[22]
-puts students[23]
 
-# total students count
+students.each do |student|
+	puts student
+end
+
+# then we count them
 puts "Overall, we have #{students.length} great students"
