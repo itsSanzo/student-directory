1063667a0f8bbe484da46d5966e074b4b6148908
itsmurasaki <itsmurasaki@gmail.com>
2014-05-16 17:30:17 +0100

    ex 8

--- directory.rb
+++ directory.rb
@@ -2,21 +2,40 @@ def input_students
   puts "Please enter the names of the students, one at a time"
   puts "(To finish, just hit return twice)"
   
+  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
   students = []
-  name = gets.chomp
+  name = gets.chomp.capitalize
 
   while !name.empty? do
+    puts "Cohort joined?"
+    cohort = gets.chomp.capitalize
+
+    while !months.include?(cohort)
+      puts "Please, don't skip the question and input a real month!"
+      cohort = gets.chomp.capitalize
+    end
+
     puts "Student's age?"
     age = gets.chomp
 
-    puts "Student's country of origin?"
-    country = gets.chomp 
+    while age.empty?
+      age = "?"
+    end
 
-    students << {:name => name, :cohort => :february, :age => age, :country => country}
+    puts "Student's country of origin?"
+    country = gets.chomp.capitalize
+
+    while country.empty?
+      puts "Please, don't skip the question!"
+      country = gets.chomp.capitalize
+    end
+
+    students << {:name => name, :cohort => cohort.to_sym, :age => age, :country => country}
 
     puts "Now we have #{students.length} students."
     puts "Add another student!"
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
 
   students
@@ -29,7 +48,7 @@ end
 
 def print(students)
   students.each do |student|
-      puts "#{student[:name].center(20)}, #{student[:age].center(2)} yo, from #{student[:country].center(10)} (#{student[:cohort]} cohort)"
+      puts "#{student[:name]}, #{student[:age]} yo, from #{student[:country]} (#{student[:cohort]} cohort)"
   end
 end
 
