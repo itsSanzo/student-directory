445be9064c538fd99fb0572d2e1120830b46f425
itsmurasaki <itsmurasaki@gmail.com>
2014-05-16 16:26:58 +0100

    added students' cohort and made the code more readable

--- directory.rb
+++ directory.rb
@@ -1,27 +1,27 @@
 students = [
-"Mario Gintili",
-"Mikhail Dubov",
-"Karolis Noreika",
-"Michael Sidon",
-"Charles De Barros",
-"Ruslan Vikhor",
-"Toby Retallick",
-"Mark Mekhaiel",
-"Sarah Young",
-"Hannah Wight",
-"Khushkaran Singh",
-"Rick brunstedt",
-"Manjit Singh",
-"Alex Gaudiosi",
-"Ross Hepburn",
-"Natascia Marchese",
-"Tiffanie Chia",
-"Matthew Thomas",
-"Freddy McGroarty",
-"Tyler Rollins",
-"Richard Curteis",
-"Anna Yanova",
-"Andrew Cumine" 
+{:name => "Mario Gintili", :cohort => :february},
+{:name => "Mikhail Dubov", :cohort => :february},
+{:name => "Karolis Noreika", :cohort => :february},
+{:name => "Michael Sidon", :cohort => :february},
+{:name => "Charles De Barros", :cohort => :february},
+{:name => "Ruslan Vikhor", :cohort => :february},
+{:name => "Toby Retallick", :cohort => :february},
+{:name => "Mark Mekhaiel", :cohort => :february},
+{:name => "Sarah Young", :cohort => :february},
+{:name => "Hannah Wight", :cohort => :february},
+{:name => "Khushkaran Singh", :cohort => :february},
+{:name => "Rick brunstedt", :cohort => :february},
+{:name => "Manjit Singh", :cohort => :february},
+{:name => "Alex Gaudiosi", :cohort => :february},
+{:name => "Ross Hepburn", :cohort => :february},
+{:name => "Natascia Marchese", :cohort => :february},
+{:name => "Tiffanie Chia", :cohort => :february},
+{:name => "Matthew Thomas", :cohort => :february},
+{:name => "Freddy McGroarty", :cohort => :february},
+{:name => "Tyler Rollins", :cohort => :february},
+{:name => "Richard Curteis", :cohort => :february},
+{:name => "Anna Yanova", :cohort => :february},
+{:name => "Andrew Cumine", :cohort => :february}
 ]
 
 def print_header
@@ -31,7 +31,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts student
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
